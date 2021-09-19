cc71619f9af43b0966a561f1a538c0213dde51bb
chrishcarter <chrishcarter@gmail.com>
2021-09-19 18:35:12 +0100

    created number of nodes script

---
+++ TD21Q3_Scripts/TD21Q3_04-2_numberOfNodes.js
@@ -0,0 +1,7 @@
+
+var number_of_nodes = node.numberOfSubNodes(node.root())
+var sceneName 		= scene.currentScene()
+var message 		= "number of subnodes in " + sceneName + " = " + number_of_nodes
+
+MessageLog.trace("\n\n" + message + "\n\n")
+//ControlCentre.printToConsole(message)
