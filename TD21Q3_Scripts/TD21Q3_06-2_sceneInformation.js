d11b186885693d379f2f5a648a07bb3731f23704
chrishcarter <chrishcarter@gmail.com>
2021-09-26 19:03:16 +0100

    Create TD21Q3_06-2_sceneInformation.js

---
+++ TD21Q3_Scripts/TD21Q3_06-2_sceneInformation.js
@@ -0,0 +1,12 @@
+/*
+input: speciffic scene
+output: text information in the message log describing elements of the scene
+
+*/
+
+
+function getSelectionInformation(){
+	MessageLog.trace("getSelectionInformation() has been clicked")
+	
+}
+
