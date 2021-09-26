924f475530b284b97b8b878413ee63a5d10b3c94
chrishcarter <chrishcarter@gmail.com>
2021-09-26 19:25:29 +0100

    formatting the write message outputs

--- TD21Q3_Scripts/TD21Q3_06-2_sceneInformation.js
+++ TD21Q3_Scripts/TD21Q3_06-2_sceneInformation.js
@@ -4,9 +4,27 @@ output: text information in the message log describing elements of the scene
 
 */
 
+function write(message){
+	MessageLog.trace(message)
+}
 
 function getSelectionInformation(){
 	MessageLog.trace("getSelectionInformation() has been clicked")
 	
+	var env_path 		= scene.currentEnvironmentPath() 
+	var proj_path 		= scene.currentProjectPath() 
+	var proj_temp_path 	= scene.tempProjectPathRemapped() 
+	var scene_start_f	= scene.getStartFrame() 
+	var scene_stop_f	= scene.getStopFrame() 
+	var scene_length 	= scene_stop_f - scene_start_f
+	
+	var outputMessage   = ""
+
+	outputMessage += ("\nEnvironment  = " + env_path)
+	outputMessage += ("\nProject          = " + proj_path)
+	outputMessage += ("\nProject TEMP = " + proj_temp_path)
+	outputMessage += ("\nTotal frames  = " + scene_length +" ["+ scene_start_f + " -> " + scene_stop_f + "]")
+	
+	write(outputMessage)
 }
 
