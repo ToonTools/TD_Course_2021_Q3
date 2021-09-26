fc572cf0cf38a04f465f4b6dc7923c8146954064
chrishcarter <chrishcarter@gmail.com>
2021-09-26 17:28:24 +0100

    Create TD21Q3_06-1_textFile_write_read.js

---
+++ TD21Q3_Scripts/TD21Q3_06-1_textFile_write_read.js
@@ -0,0 +1,16 @@
+// set the name of the text file
+
+
+// make text file
+function textFile_write(){
+	MessageLog.trace("textFile_make button pressed")
+	
+}
+
+
+// read text file
+function textFile_read(){
+	
+	MessageLog.trace("textFile_read button pressed")
+	
+}
