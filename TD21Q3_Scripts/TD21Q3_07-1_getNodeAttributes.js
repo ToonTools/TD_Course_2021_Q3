6678ab43d33aeda94ce81604f81badcdac137f82
chrishcarter <chrishcarter@gmail.com>
2021-10-02 17:54:10 +0100

    wrote pseudocode

--- TD21Q3_Scripts/TD21Q3_07-1_getNodeAttributes.js
+++ TD21Q3_Scripts/TD21Q3_07-1_getNodeAttributes.js
@@ -5,7 +5,16 @@ output: text information of all of that nodes attributes and their values
 */
 
 function getNodeAttributes(){
-
+	MessageLog.trace("getNodeAttributes started")
+	
+	// check that only one node has been selected
+	
+	// check if that node has any attributes
+	
+	// if it does have attributes, then go through each attribute and display its name and its value
+	
+	
+	
 
 
 
