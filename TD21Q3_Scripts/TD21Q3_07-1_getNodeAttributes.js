74579ca18f605690bd73772596fb67e86eb60bad
chrishcarter <chrishcarter@gmail.com>
2021-10-02 18:01:37 +0100

    added fail conditions for too many and too few nodes selected

--- TD21Q3_Scripts/TD21Q3_07-1_getNodeAttributes.js
+++ TD21Q3_Scripts/TD21Q3_07-1_getNodeAttributes.js
@@ -7,6 +7,19 @@ output: text information of all of that nodes attributes and their values
 function getNodeAttributes(){
 	MessageLog.trace("getNodeAttributes started")
 	
+	if ( selection.numberOfNodesSelected() > 1 ){
+		MessageLog.trace("you have too many nodes selected, just select one and try again")
+		return
+	}
+	else if (selection.numberOfNodesSelected() <= 0 ){
+		MessageLog.trace("you have no nodes selected, just select one and try again")
+		return
+	}
+	
+	var myNode = selection.selectedNode(0)
+	MessageLog.trace(myNode)
+	
+	
 	// check that only one node has been selected
 	
 	// check if that node has any attributes
