19f531a7f8f306e183fdf306684b9a12af1368b5
chrishcarter <chrishcarter@gmail.com>
2021-10-02 19:33:16 +0100

    creates composite

--- TD21Q3_Scripts/TD21Q3_07-2_addComposite.js
+++ TD21Q3_Scripts/TD21Q3_07-2_addComposite.js
@@ -8,4 +8,19 @@ How to use:
 Note: very minimal validation in this script so there will be many use cases where this does not work
 */
 
-
+function addComposite(){
+	MessageLog.trace("--- addComposite() called ---")
+	
+	// build composite	
+	var parentGroup = "Top"
+	var nodeName 	= "myComposite"
+	var nodeType 	= "COMPOSITE"
+	var node_x		= 0
+	var node_y		= 0
+	var node_z		= 0
+	
+	node.add( parentGroup , nodeName, nodeType , node_x , node_y , node_z )
+	
+	
+	MessageLog.trace("--- addComposite() completed  ---")
+}
