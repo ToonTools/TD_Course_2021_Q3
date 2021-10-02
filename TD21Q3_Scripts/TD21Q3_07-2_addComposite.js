a38bac750bf2512d9d2d9305a9d1baee9dc84191
chrishcarter <chrishcarter@gmail.com>
2021-10-02 19:27:39 +0100

    Create TD21Q3_07-2_addComposite.js

---
+++ TD21Q3_Scripts/TD21Q3_07-2_addComposite.js
@@ -0,0 +1,11 @@
+/*
+Input : one node in a scene
+Output: adds a composite node to that element
+
+How to use: 
+    within Harmony :  add addComposite() as a button. Select a node. Press the button, composite will be added
+
+Note: very minimal validation in this script so there will be many use cases where this does not work
+*/
+
+
