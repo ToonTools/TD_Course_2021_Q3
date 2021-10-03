cbf3d041ca1e90e5317d5475b52d21f2c049f9c3
chrishcarter <chrishcarter@gmail.com>
2021-10-03 18:51:33 +0100

    Create TD21Q3_08-2_addWriteNodes_toComposite.js

---
+++ TD21Q3_Scripts/TD21Q3_08-2_addWriteNodes_toComposite.js
@@ -0,0 +1,37 @@
+/*
+Input : any harmony scene with speciffically named composite nodes
+Output: adds write nodes to upstream nodes of the speciffic composite nodes
+
+How to use: 
+    choose the comp node name you want to add write nodes to and choose a write node export Location
+*/
+
+function addWriteNodes_toComposite(){
+	scene.beginUndoRedoAccum("---");
+	MessageLog.trace("addWriteNodes_toComposite : Started")
+	
+	
+	// define compositing node we want to recognise
+
+	// define export type
+
+	// define export path
+
+	// define export naming convention
+		
+	
+	
+	MessageLog.trace("addWriteNodes_toComposite : Completed")
+	scene.endUndoRedoAccum()
+}
+
+
+
+
+
+
+
+
+
+
+
