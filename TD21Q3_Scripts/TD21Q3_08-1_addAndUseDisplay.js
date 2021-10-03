2b5818a386b5ba0cfaf134e2b99d71623df539ed
chrishcarter <chrishcarter@gmail.com>
2021-10-03 18:00:53 +0100

    builds a composite at the same hierarchy level as the selection

--- TD21Q3_Scripts/TD21Q3_08-1_addAndUseDisplay.js
+++ TD21Q3_Scripts/TD21Q3_08-1_addAndUseDisplay.js
@@ -11,6 +11,48 @@ function addAndUserDisplays(){
     scene.beginUndoRedoAccum("addAndUseDisplays()")
 	MessageLog.trace("\t -- started: addAndUseDisplays()")
 
+    // get current selection, reject if nothing is selected
+	var userSelection = selection.selectedNodes()
+	if(userSelection.length <= 0){
+		MessageLog.trace("nothing selected, will end process")
+		return
+	}
+
+    // -------------
+    // TODO ----------- might add in validation later on 
+    // -------------
+
+    var firstNodeInSelection 	= userSelection[0]
+	var firstNodeName 			= node.getName(firstNodeInSelection)
+	var firstNodeParent			= node.parentNode(firstNodeInSelection)
+
+    // calculate average x pos of selection
+	// calulcate min y pos of selection
+	var total_x = 0
+	var min_y 	= -999
+
+	for( n in userSelection ){
+
+		var sel_x 	= node.coordX(userSelection[n])
+		total_x 	+= sel_x 
+
+		var sel_y = node.coordY(userSelection[n])
+		if( sel_y > min_y ){
+			min_y = sel_y
+		}
+	}
+
+	var average_x   = total_x/userSelection.length
+	var nodeParent  = firstNodeParent + "/"
+	var compName    = firstNodeName + "_CMP"
+	var offset_y    = 100
+	var comp_x      = average_x
+	var comp_y      = min_y + offset_y
+	var comp_z      = 0
+
+    var newCompositeNode = node.add(nodeParent, compName, "COMPOSITE", comp_x, comp_y, comp_z)
+    // set composite type to bitmap on creation
+    node.setTextAttr(newCompositeNode, "compositeMode", 1, "compositeBitmap");
 
 	MessageLog.trace("\t -- completed: addAndUseDisplays()")
 	scene.endUndoRedoAccum()
