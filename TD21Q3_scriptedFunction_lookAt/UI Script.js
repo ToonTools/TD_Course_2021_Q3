18545a26de37afb923634e5108c3367078b9240d
chrishcarter <chrishcarter@gmail.com>
2021-10-16 19:48:23 +0100

    added try catch to clarify  errors

--- TD21Q3_scriptedFunction_lookAt/UI Script.js
+++ TD21Q3_scriptedFunction_lookAt/UI Script.js
@@ -1,18 +1,24 @@
 function onPoint2dValueChanged(params, point2dVal)
 {
-    MessageLog.trace("START")
-  var targetAttr = params.targetAttrs[0];
-    MessageLog.trace("Attribute Name: " + targetAttr)
-  for(var i=0; i<params.targetNodes.length; ++i)
-  {
-    var targetNode = params.targetNodes[i];
-    MessageLog.trace("------------------")
-    MessageLog.trace("Node INdex : " + i)
-    Messagelog.trace("Node Identifier: " + targetNode)
+    try{
+        MessageLog.trace("START")
+        var targetAttr = params.targetAttrs[0];
+        MessageLog.trace("Attribute Name: " + targetAttr)
+        for(var i=0; i<params.targetNodes.length; ++i){
+    
+            var targetNode = params.targetNodes[i];
+            MessageLog.trace("------------------")
+            MessageLog.trace("Node Index : " + i)
+            MessageLog.trace("Node Identifier: " + targetNode)
+    
+        }
+        MessageLog.trace("END")
 
-  }
-    MessageLog.trace("END")
+    }
+    catch(error){
+        MessageLog.trace("ERROR : " + error)
+    }
+
 }
-
 
 include(fileMapper.toNativePath(specialFolders.resource+"/scripts/utilities/ui/functionWizard/mcs/mcPoint2dFunction.js"))
