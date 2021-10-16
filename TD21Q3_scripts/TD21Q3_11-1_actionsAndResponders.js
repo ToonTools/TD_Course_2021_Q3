4c2fa7bfbcf41848769c73586d10f9a026e71009
chrishcarter <chrishcarter@gmail.com>
2021-10-16 20:29:28 +0100

    Update TD21Q3_11-1_actionsAndResponders.js

--- TD21Q3_scripts/TD21Q3_11-1_actionsAndResponders.js
+++ TD21Q3_scripts/TD21Q3_11-1_actionsAndResponders.js
@@ -1,16 +1,18 @@
 //https://docs.toonboom.com/help/harmony-20/scripting/script/classAction.html#ad9b65553c0959c0b7295a448f7bd1e3e
 
 
-var rList = Action.getResponderList();
-for(var i=0; i < rList.length; ++i)
-{
-  MessageLog.trace( "ResponderIdentity=" + rList[i] );
+function getResponderList(){
+  var rList = Action.getResponderList();
+  for(var i=0; i < rList.length; ++i){
+    var selResponder = rList[i] 
+    MessageLog.trace( "ResponderIdentity = " + selResponder );
+  }
 }
 
-/*
-var aList = Action.getActionList("Node View");
-for(var i=0; i < aList.length; ++i)
-{
-  MessageLog.trace( "availableAction=" + aList[i] );
+function getActionList(){
+
+  var aList = Action.getActionList("Node View");
+  for(var i=0; i < aList.length; ++i){
+    MessageLog.trace( "availableAction =" + aList[i] );
+  }
 }
-*/
