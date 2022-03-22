923defb5d46073548bc01e936277ac74e7e11abd
chrishcarter <chrishcarter@gmail.com>
2022-03-22 16:08:35 +0000

    added way to add shortcuts in packages

---
+++ TD21Q3_packages/TD21Q3_customShortcut/configure.js
@@ -0,0 +1,34 @@
+function configure()
+{
+  if(about.isStageEssentials() || about.isStageAdvanced() || about.isPaintMode())
+    return;
+
+    addShortcut()
+}
+
+function addShortcut(){
+    msg_funcName		= 	"addShortcut"
+    msg_start 		= 	" --- Started : " + msg_funcName + "  --- " 
+    msg_end  			= 	" --- Finished : " + msg_funcName + "  --- " 
+  
+    MessageLog.trace(msg_start)
+  
+    MessageLog.trace( JSON.stringify(ScriptManager,null,2) )
+  
+  
+    
+    ScriptManager.addShortcut( { 	text     		: "get node attributes",
+                    id       		: "getNodeAttributes",
+                    longDesc 		: "gets attribute information for selected node",
+                    action   		: "numberOfNodes in TD21Q3_04-2_numberOfNodes.js",
+                    order    		: "256",
+                    categoryId   	: "TD_Custom", 
+                    categoryText 	: "TD_Custom",
+                    value 			: "#" 
+                  })
+    
+    MessageLog.trace(msg_end)
+
+}
+
+exports.configure = configure;
